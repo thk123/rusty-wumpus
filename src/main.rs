3fc06c9447618fdc8b58189f6b1fd7fd67287d6c
Thomas Kiley <thomas.kiley@diffblue.com>
2020-07-13 14:30:11 +0100

    Adding rooms
    This is a commit done to backup WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use rand::Rng;
+
+struct Room {
+    room_index: u32,
+    connected_rooms: Vec<u32>,
+}
+
+struct Map {
+    map : Vec<Room>,
+
+}
+
+impl Map {
+
+    fn create(size: u32) -> Map {
+        Map {
+            map: (0..size).map(|node_index| Room{room_index:node_index, connected_rooms: Vec::new()}).collect()
+        }
+    }
+
+    fn debug_print(&self) {
+        println!("Map size: {}", self.map.len());
+        for room in &self.map {
+            println!("[{}]", room.room_index)
+        }
+
+    }
+
+
+}
+
+#[cfg(test)]
+mod tests
+{
+    #[test]
+    fn map_create() {
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let map = Map::create(25);
+    map.debug_print();
 }
